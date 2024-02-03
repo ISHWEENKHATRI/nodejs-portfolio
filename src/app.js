28f3856b9aee7bc18ef944dfce48b5fe8b3045b2
ISHWEENKHATRI <ishween.khatri.csai.20@ggits.net>
2024-02-03 22:10:21 +0530

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const express=require("express");
 const hbs=require("hbs");
 const app=express();
+const mongoose=require('mongoose');
 const routes=require('./routes/main');
 
+app.use(express.static("public"));
 app.use('',routes);
 
 //(template engine)
 app.set('view engine','hbs');
 app.set("views","views");
+hbs.registerPartials("views/partials")
+
+//db connection
+mongoose.connect("mongodb://localhost/nodejs_portfolio")
+    .then(()=>{
+        console.log("Database Connected");
+    })
+    .catch((error)=>{
+        console.error("Error connecting to Database: ",error);
+    });
 
 app.listen(process.env.PORT || 2000,()=>{
     console.log("Server Started");
